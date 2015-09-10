851082e997e9bcf714162d4e7c5e6cf3f2437e4f
Sevatec <github@sevatec.com>
2015-09-10 10:13:47 -0400

    First commit

---
+++ src/main/java/PetClinic_Selenium/PetClinic_Selenium/App.java
@@ -0,0 +1,13 @@
+package PetClinic_Selenium.PetClinic_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PetClinic_Selenium/PetClinic_Selenium/SeleniumTest.java
@@ -0,0 +1,82 @@
+package PetClinic_Selenium.PetClinic_Selenium;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SeleniumTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( SeleniumTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    
+    public void testApp()
+    {
+    	System.out.println("Test test 2");
+        assertTrue( true );
+    }
+    
+    public void testGoogle(){
+    	// Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Wine!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("wine!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+        Assert.assertTrue(driver.getTitle().toLowerCase().startsWith("wine!"));
+        //Close the browser
+        driver.close();
+    }
+}
