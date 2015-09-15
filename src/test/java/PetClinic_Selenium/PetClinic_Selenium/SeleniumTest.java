87a16c3203b794031132bb7c52d43792fc3d3a74
Sevatec <github@sevatec.com>
2015-09-15 13:03:09 -0400

    First PetClinic test

--- src/test/java/PetClinic_Selenium/PetClinic_Selenium/SeleniumTest.java
+++ src/test/java/PetClinic_Selenium/PetClinic_Selenium/SeleniumTest.java
@@ -34,22 +34,15 @@ public class SeleniumTest extends TestCase
 
     /**
      * Rigourous Test :-)
-     */
-    
-    public void testApp()
-    {
-    	System.out.println("Test test 2");
-        assertTrue( true );
-    }
-    
+     */    
     public void testGoogle(){
     	// Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface, 
         // not the implementation.
         WebDriver driver = new FirefoxDriver();
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
+        // And now use this to visit webpage
+        driver.get("http://54.172.164.220:8080/petclinic");
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
@@ -57,10 +50,10 @@ public class SeleniumTest extends TestCase
         WebElement element = driver.findElement(By.name("q"));
 
         // Enter something to search for
-        element.sendKeys("Wine!");
+        //element.sendKeys("Wine!");
 
         // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+        //element.submit();
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
@@ -69,13 +62,13 @@ public class SeleniumTest extends TestCase
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("wine!");
+                return d.getTitle().toLowerCase().startsWith("PetClinic");
             }
         });
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
-        Assert.assertTrue(driver.getTitle().toLowerCase().startsWith("wine!"));
+        Assert.assertTrue(driver.getTitle().toLowerCase().startsWith("PetClinic"));
         //Close the browser
         driver.close();
     }
